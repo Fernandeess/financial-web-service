7ffa9029a1e24ddc2355e7a9f0a5a55e9c4e5182
matheusfernandesrodrigues <matheusfernandesrodrigues@outlook.com>
2025-02-16 19:03:19 -0300

    refact: implements @input() on card-info

--- src/app/components/card-info/card-info.component.ts
+++ src/app/components/card-info/card-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-info',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './card-info.component.html',
   styleUrl: './card-info.component.scss',
 })
-export class CardInfoComponent {}
+export class CardInfoComponent {
+  @Input() tipo: string = 'EXEMPLO';
+  @Input() value: number = 0;
+}
