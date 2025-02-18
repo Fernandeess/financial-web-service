a3aa273d24931dc9aa3e1217978edcb8df810bb9
matheusfernandesrodrigues <matheusfernandesrodrigues@outlook.com>
2025-02-18 14:52:45 -0300

    add card page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { authGuard } from './auth.guard';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './pages/card/card.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'cards', component: CardComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: 'login' },
 ];
